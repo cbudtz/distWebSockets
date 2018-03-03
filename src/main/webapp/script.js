95bca02538b0faae64920cd0c9f714da6a12b7b8
cbudtz <chrbudtz@gmail.com>
2018-03-03 21:39:06 +0100

    gitignore and support for SSL

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,7 +1,11 @@
 var socket;
 function connect(){
     var id = document.getElementById("clientid").value;
-    socket = new WebSocket("ws://" + window.location.host + "/chat/" + id);
+    let protocol = "ws://"
+    if (window.location.protocol==="https:"){
+        protocol = "wss://";
+    }
+    socket = new WebSocket(protocol + window.location.host + "/chat/" + id);
     socket.onmessage = (e)=>{
         document.getElementById("output").innerHTML += "<BR>" + e.data;
     }
